3569a55cc40a779e479f3bc3f236623323c1eb82
Juzantino Junadi <juzantinojunadi@Juzantinos-MacBook-Pro.local>
2022-09-19 16:00:58 +0800

    first commit

---
+++ Q1.py
@@ -0,0 +1,3 @@
+#Demonstrate your understanding of how class hierarchy and association are 
+#used to organize information, and then construct the 
+#class hierarchy and association according to specification.
