e1e181f5b583cee4d7a0672903f71a1debcae547
Juzantino Junadi <juzantinojunadi@Juzantinos-MacBook-Pro.local>
2022-09-23 20:37:26 +0800

    solving qns 2

---
+++ Q2.py
@@ -0,0 +1,8 @@
+
+class Shaft():
+    def __init__(self, length:float,weight:int,material:str,flex:str):
+        self._length = length
+        self._weight = weight
+        self._material = material
+        self._flex = flex
+        
